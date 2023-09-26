982cefaa742c18b46ead624bf66483f5d63fc3a8
Reinandafaris <reinandafari7@gmail.com>
2023-09-26 18:06:29 +0700

    first commit

---
+++ app/_app.tsx
@@ -0,0 +1,25 @@
+import Head from 'next/head';
+import './globals.css';
+import { AppProps } from 'next/app';
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
+        <meta name="description" content="CRUD-Nextjs" />
+        <meta name="keywords" content="CRUD-Nextjs" />
+        <title>CRUD ~ Next.js</title>
+
+        <link rel="manifest" href="/manifest.json" />
+        <link href="/icons/favicon-16x16.png" rel="icon" type="image/png" sizes="16x16" />
+        <link href="/icons/favicon-32x32.png" rel="icon" type="image/png" sizes="32x32" />
+        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+        <meta name="theme-color" content="#000000" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
---
+++ components/Hero.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <div className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img">
+      <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/50 z-[2]" />
+      <div className="p-5 text-white z-[2] ml-[-10rem] mt-[-10rem]">
+        <h2 className="text-5xl font-bold">Selamat Datang!</h2>
+        <p className="py-5 text-xl">Ini Adalah Aplikasi CRUD - NEXT.JS</p>
+        <Link href="/products">
+          <button type="button" className="btn">
+            Mulai!
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
